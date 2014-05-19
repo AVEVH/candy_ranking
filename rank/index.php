ae81fc182b8401450481819fd1fd3046f406c883
yuki abe <ave@avevh.com>
2014-05-19 11:39:00 +0900

    candy

---
+++ rank/index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<meta http-equiv="Content-Script-Type" content="text/javascript">
+<meta name="robots" content="INDEX,FOLLOW,ALL"> 
+<meta http-equiv="imagetoolbar" content="no">
+<meta name="keywords" content="TOP CANDY,歌舞伎町,TOP DANDY,求人,求人特設サイト,CANDY,ホストクラブ," />
+<meta name="description" content="ついに！TOP DANDYから兄弟店『TOP CANDY』オープン！オープニングスタッフ募集中！" />
+<meta property="og:title" content="TOP CANDY | オフィシャルサイト">
+<meta property="og:type" content="website" >
+<meta property="og:url" content="フェイスブック用">
+<meta property="og:description" content="ついに！TOP DANDYから兄弟店『TOP CANDY』オープン！オープニングスタッフ募集中！">
+<title>TOP CANDY│オフィシャルサイト</title>
+
+<link href='http://fonts.googleapis.com/css?family=Roboto:100' rel='stylesheet' type='text/css'>
+<link href="/css_official/reset.css" rel="stylesheet" type="text/css" />
+<link href="/css_official/main.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="/css_official/slideshow.css"/>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+<!-- ie7 -->
+<script src="/js_official/selectivizr.js"></script>
+<!--[if lt IE 8]>
+<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE8.js"></script>
+<![endif]-->
+<!--[if lt IE 9]>
+<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+</head>
+<body>
+<img src="/images_official/bg.jpg" alt="bg" width="2000" height="1655" class="bg"/>
+
+<div id="container">
+<!-- ここからhaeder -->
+<div id="header_area">
+<?php
+	include('/home/ogikuboman/host-candy.jp/public_html/inc_official/header.txt');
+?>
+</div>
+<!-- ここまでhaeder -->
+
+<div id="contents">
+<!-- ここからleft -->
+<div id="left">
+<?php
+	include('/home/ogikuboman/host-candy.jp/public_html/inc_official/left.txt');
+?>
+</div>
+<!-- ここまでleft -->
+
+<!-- ここからmain contents -->
+<div id="main_contents">
+  
+  <!-- ここからranking -->
+  <div id="top_ranking">
+	  <div id="top_title_ranking">
+		  <img src="/images_official/title_ranking.jpg" alt="ランキング" width="265" height="86" />
+	  </div>
+	  <!-- ▼ここからランキングスタッフ▼ -->
+	  
+    <?php
+    //$mode = htmlspecialchars($_GET['mode'], ENT_QUOTES);
+    include("http://fm.ogikuboman.com/fmi/xsl/candy/pc_rank.xsl?-db=mens_menu_candy&-lay=web&-sortfield.1=rank_sale&-max=10&hp_on=1&-find");
+    ?>
+	  
+	  <!-- ▲ここまでランキングスタッフ▲ -->
+	  
+	  <div id="ranking_staff_detail">
+		  <p><a href="/staff/">スタッフ一覧はこちら</a></p>
+	  </div>
+  </div>
+  <!-- ここまでranking -->
+
+</div>
+<!-- ここまでmain contents -->
+</div><!-- ここまでcontents -->
+
+<!-- ここからfooter -->
+<div id="footer">
+<?php
+	include('/home/ogikuboman/host-candy.jp/public_html/inc_official/footer.txt');
+?>
+</div>
+<!-- ここまでfooter -->
+</div><!-- ここまでcontainer -->
+</body>
+</html>
---
+++ rank/index_bk.php
@@ -0,0 +1,204 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8">
+<meta http-equiv="Content-Style-Type" content="text/css">
+<meta http-equiv="Content-Script-Type" content="text/javascript">
+<meta name="robots" content="INDEX,FOLLOW,ALL"> 
+<meta http-equiv="imagetoolbar" content="no">
+<meta name="keywords" content="TOP CANDY,歌舞伎町,TOP DANDY,求人,求人特設サイト,CANDY,ホストクラブ," />
+<meta name="description" content="ついに！TOP DANDYから兄弟店『TOP CANDY』オープン！オープニングスタッフ募集中！" />
+<meta property="og:title" content="TOP CANDY | オフィシャルサイト">
+<meta property="og:type" content="website" >
+<meta property="og:url" content="フェイスブック用">
+<meta property="og:description" content="ついに！TOP DANDYから兄弟店『TOP CANDY』オープン！オープニングスタッフ募集中！">
+<title>TOP CANDY│オフィシャルサイト</title>
+
+<link href='http://fonts.googleapis.com/css?family=Roboto:100' rel='stylesheet' type='text/css'>
+<link href="/css_official/reset.css" rel="stylesheet" type="text/css" />
+<link href="/css_official/main.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="/css_official/slideshow.css"/>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+
+<!-- ie7 -->
+<script src="/js_official/selectivizr.js"></script>
+<!--[if lt IE 8]>
+<script src="http://ie7-js.googlecode.com/svn/version/2.1(beta4)/IE8.js"></script>
+<![endif]-->
+
+<!--[if lt IE 9]>
+<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<!-- ページ移管 
+<script src="http://cdn.jsdelivr.net/jquery.easing/1.3/jquery.easing.1.3.js"></script>
+<script src="/js_official/jquery.clickstream.min.js"></script>
+<script type="text/javascript">
+$j=jQuery.noConflict();
+
+$j(document).ready(function() {
+
+    $j("body").clickstream({
+
+        //遷移時のスピード。
+            inSpeed:          800,
+            outSpeed:         800,
+
+            //遷移時のディレイ時間。
+            inDelay :         100,
+            outDelay :        100,
+
+            //アニメーションさせずに遷移させたい場合に、そのaタグに付けるクラス名。
+            inactiveClass :   'clickstream_inactive',
+
+            //アニメーションの種類。
+            inEffect:         'rightToLeft', // or 'leftToRight','rightToLeft','topToBottom','bottomToTop'
+            outEffect:        'rightToLeft', // or 'leftToRight','rightToLeft','topToBottom','bottomToTop'
+
+            //アニメーションのイージングの種類。
+            //jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
+            inEasing:         'easeOutExpo',
+            outEasing:        'easeOutExpo',
+
+            //画面IN直前とOUT直前の透過量
+            defaultOpacity:   0,
+            outOpacity:       0,
+
+            //画面IN直前のポジション（fade選択時以外の時に適応）
+            defaultPositinX:  '10%',
+            defaultPositinY:  '10%',
+
+            //画面OUT直前のポジション（fade選択時以外の時に適応）
+            outPositinX:      '10%',
+            outPositinY:      '10%',
+
+    });
+
+}); 
+</script>-->
+
+</head>
+<body>
+<img src="/images_official/bg.jpg" alt="bg" width="2000" height="1655" class="bg"/>
+
+<div id="container">
+<!-- ここからhaeder -->
+<div id="header_area">
+<?php
+	include('/home/ogikuboman/host-candy.jp/public_html/inc_official/header.txt');
+?>
+</div>
+<!-- ここまでhaeder -->
+
+<div id="contents">
+<!-- ここからleft -->
+<div id="left">
+<?php
+	include('/home/ogikuboman/host-candy.jp/public_html/inc_official/left.txt');
+?>
+</div>
+<!-- ここまでleft -->
+
+<!-- ここからmain contents -->
+<div id="main_contents">
+  
+  <!-- ここからranking -->
+  <div id="top_ranking">
+	  <div id="top_title_ranking">
+		  <img src="/images_official/title_ranking.jpg" alt="ランキング" width="265" height="86" />
+	  </div>
+	  <!-- ▼ここからランキングスタッフ▼ -->	  
+	  <div class="top_staff_ranking_box">
+		  <img src="/images_official/no1.jpg" width="106" height="238" /><img src="/images_official/test1.jpg" width="159" height="238" />
+		  <div class="top_staff_ranking_txt">
+			  <span class="ranking_post">Playing Manager</span><span class="ranking_name">嶋村 拓斗</span>
+		  </div>
+	  </div>
+	  
+	  <div class="top_staff_ranking_box">
+		  <img src="/images_official/no2.jpg" width="106" height="238" /><img src="/images_official/test1.jpg" width="159" height="238" />
+		  <div class="top_staff_ranking_txt">
+			  <span class="ranking_post">Playing Manager</span><span class="ranking_name">嶋村 拓斗</span>
+		  </div>
+	  </div>
+	  
+	  <div class="top_staff_ranking_box">
+		  <img src="/images_official/no3.jpg" width="106" height="238" /><img src="/images_official/test1.jpg" width="159" height="238" />
+		  <div class="top_staff_ranking_txt">
+			  <span class="ranking_post">Playing Manager</span><span class="ranking_name">嶋村 拓斗</span>
+		  </div>
+	  </div>
+	  
+	  <div class="top_staff_ranking_box">
+		  <img src="/images_official/no4.jpg" width="106" height="238" /><img src="/images_official/test1.jpg" width="159" height="238" />
+		  <div class="top_staff_ranking_txt">
+			  <span class="ranking_post">Playing Manager</span><span class="ranking_name">嶋村 拓斗</span>
+		  </div>
+	  </div>
+	  
+	  <div class="top_staff_ranking_box">
+		  <img src="/images_official/no5.jpg" width="106" height="238" /><img src="/images_official/test1.jpg" width="159" height="238" />
+		  <div class="top_staff_ranking_txt">
+			  <span class="ranking_post">Playing Manager</span><span class="ranking_name">嶋村 拓斗</span>
+		  </div>
+	  </div>
+	  
+	  <div class="top_staff_ranking_box">
+		  <img src="/images_official/no6.jpg" width="106" height="238" /><img src="/images_official/test1.jpg" width="159" height="238" />
+		  <div class="top_staff_ranking_txt">
+			  <span class="ranking_post">Playing Manager</span><span class="ranking_name">嶋村 拓斗</span>
+		  </div>
+	  </div>
+	  
+	  <div class="top_staff_ranking_box">
+		  <img src="/images_official/no7.jpg" width="106" height="238" /><img src="/images_official/test1.jpg" width="159" height="238" />
+		  <div class="top_staff_ranking_txt">
+			  <span class="ranking_post">Playing Manager</span><span class="ranking_name">嶋村 拓斗</span>
+		  </div>
+	  </div>
+	  
+	  <div class="top_staff_ranking_box">
+		  <img src="/images_official/no8.jpg" width="106" height="238" /><img src="/images_official/test1.jpg" width="159" height="238" />
+		  <div class="top_staff_ranking_txt">
+			  <span class="ranking_post">Playing Manager</span><span class="ranking_name">嶋村 拓斗</span>
+		  </div>
+	  </div>
+	  
+	  <div class="top_staff_ranking_box">
+		  <img src="/images_official/no9.jpg" width="106" height="238" /><img src="/images_official/test1.jpg" width="159" height="238" />
+		  <div class="top_staff_ranking_txt">
+			  <span class="ranking_post">Playing Manager</span><span class="ranking_name">嶋村 拓斗</span>
+		  </div>
+	  </div>
+	  
+	  <div class="top_staff_ranking_box">
+		  <img src="/images_official/no10.jpg" width="106" height="238" /><img src="/images_official/test1.jpg" width="159" height="238" />
+		  <div class="top_staff_ranking_txt">
+			  <span class="ranking_post">Playing Manager</span><span class="ranking_name">嶋村 拓斗</span>
+		  </div>
+	  </div>
+	  <!-- ▲ここまでランキングスタッフ▲ -->
+	  
+	  <div id="ranking_staff_detail">
+		  <p><a href="/staff/">スタッフ一覧はこちら</a></p>
+	  </div>
+  </div>
+  <!-- ここまでranking -->
+
+</div>
+<!-- ここまでmain contents -->
+</div><!-- ここまでcontents -->
+
+<!-- ここからfooter -->
+<div id="footer">
+<?php
+	include('/home/ogikuboman/host-candy.jp/public_html/inc_official/footer.txt');
+?>
+</div>
+<!-- ここまでfooter -->
+</div><!-- ここまでcontainer -->
+</body>
+</html>
